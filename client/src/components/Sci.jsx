aea53a317f87b5550199a9da8aba5589bed93271
Gustavo <gustavoaugustocar@hotmail.com>
2022-09-09 10:18:12 -0300

    Changing sci components

--- client/src/components/Sci.jsx
+++ client/src/components/Sci.jsx
@@ -4,33 +4,23 @@ import { BsLinkedin, BsInstagram, BsGithub } from "react-icons/bs";
 
 const Sci = () => {
     return (
-        <ul className="app_social">
-            <li className="socialItem">
-                <a href="https://github.com/Guxxtavoww" target="_blank" rel="noopener noreferrer" className="link-sci">
-                    <BsGithub />
-                </a>    
-            </li>
-            <li className="socialItem">
-                <a href="https://www.linkedin.com/in/gustavo-augusto-3a513b1b5/" target="_blank" rel="noopener noreferrer" className="link-sci">
-                    <BsLinkedin />
-                </a>    
-            </li>
-            <li className="socialItem">
-                <a href="https://www.instagram.com/web_dev_freelancer/" target="_blank" rel="noopener noreferrer" className="link-sci">
-                    <BsInstagram />
-                </a>    
-            </li>
-            <li className="socialItem">
-                <a href="https://www.facebook.com/profile.php?id=100078833855498" target="_blank" rel="noopener noreferrer" className="link-sci">
-                    <FaFacebookF />
-                </a>    
-            </li>
-            <li className="socialItem">
-                <a href="https://api.whatsapp.com/send?phone=5511989459239&text=Ol%C3%A1%20!%20Gostaria%20de%20fazer%20uma%20proposta%20para%20um%20site%2C%20script%20etc..." target="_blank" rel="noopener noreferrer" className="link-sci">
-                    <FaWhatsapp />
-                </a>    
-            </li>
-        </ul>
+        <div className="app_social">
+            <a href="https://github.com/Guxxtavoww" target="_blank" rel="noopener noreferrer" className=" socialItem">
+                <BsGithub />
+            </a>
+            <a href="https://www.linkedin.com/in/gustavo-augusto-3a513b1b5/" target="_blank" rel="noopener noreferrer" className=" socialItem">
+                <BsLinkedin />
+            </a>
+            <a href="https://www.instagram.com/web_dev_freelancer/" target="_blank" rel="noopener noreferrer" className=" socialItem">
+                <BsInstagram />
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=100078833855498" target="_blank" rel="noopener noreferrer" className=" socialItem">
+                <FaFacebookF />
+            </a>
+            <a href="https://api.whatsapp.com/send?phone=5511989459239&text=Ol%C3%A1%20!%20Gostaria%20de%20fazer%20uma%20proposta%20para%20um%20site%2C%20script%20etc..." target="_blank" rel="noopener noreferrer" className=" socialItem">
+                <FaWhatsapp />
+            </a>
+        </div>
     );
 }
 
